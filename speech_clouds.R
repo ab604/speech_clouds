5cd66bb27bddaff3d26c887a6a7d2074562b7b7a
ab604 <ab604@soton.ac.uk>
2017-10-04 14:44:06 +0100

    Update images

--- speech_clouds.R
+++ speech_clouds.R
@@ -38,7 +38,7 @@ maytdm <- as.matrix(maytdm[order(maytdm,decreasing = TRUE),])
 # Make wordcloud ---------------------------------------------------------------
 set.seed(1234)
 #pdf("speechcloud.pdf")
-png("images/speechcloud.png")
+png("images/speechcloud.png",width = 20, height = 20, units = "cm", res =600)
 par(mfrow=c(1,2))
 wordcloud(rownames(corbtdm),corbtdm, min.freq = 1, scale=c(5, .2),
           max.words=200, random.order=FALSE, rot.per=0.35, 
@@ -51,14 +51,16 @@ dev.off()
 
 
 #pdf("comparison_cloud.pdf")
-png("images/comparison_cloud.png")
+png("images/comparison_cloud.png",width = 20, height = 20, 
+    units = "cm", res =600)
 par(mfrow=c(1,1))
 comparison.cloud(tdm, random.order=FALSE, 
                  colors = c("indianred3","lightsteelblue3"), 
                  title.size=2.5, max.words=400)
 dev.off()
 
-png("images/commonality_cloud.png")
+png("images/commonality_cloud.png",width = 20, height = 20, 
+    units = "cm", res =600)
 par(mfrow=c(1,1))
 commonality.cloud(tdm, 
                   random.order=FALSE, 
